186cf0d993bda90bc4c653258a4ed34923c78c06
Brandon Mathis <brandon@imathis.com>
2018-05-23 16:31:16 -0500

    Added comments, test for fills, test for disabling slider labels

--- test/slider.test.js
+++ test/slider.test.js
@@ -3,6 +3,7 @@ var u = require('./_utils.js')
 beforeAll(async () => {
   await page.goto("http://localhost:8081/sliders.html")
 })
+
 describe('Browser slider testing', () => {
 
   it('matches the initial state', async () => {
@@ -26,12 +27,15 @@ describe('Browser slider testing', () => {
   })
 
   it('can track input changes with sliderState', async () => {
+    // Default is 50 so 49 should be a decreased state
     await u.setValue('#slider-1 input', '49')
     expect(await u.data('#slider-1 div', 'sliderState')).toBe('decreased')
-    
+
+    // Default is 50 so 51 should be an increased state
     await u.setValue('#slider-1 input', '51')
     expect(await u.data('#slider-1 div', 'sliderState')).toBe('increased')
 
+    // Default is 50 so 50 should be the initial state
     await u.setValue('#slider-1 input', '50')
     expect(await u.data('#slider-1 div', 'sliderState')).toBe('initial')
   })
@@ -40,19 +44,41 @@ describe('Browser slider testing', () => {
     await u.matchText('#slider-1', 'Disk: 50MB')
   })
 
+  it('fills slider track', async () => {
+    // Set the slider to 50
+    await u.setValue('#slider-1 input', '50')
+
+    // Ensure that there are 50 filled slider-track elements
+    await u.countIs('#slider-1 .slider-fill.filled', 50)
+
+    // Slider fill segment 50 should be filled, but not 51
+    await u.findElement('#slider-1 .slider-fill.filled[data-index="50"]')
+    await u.findElement('#slider-1 .slider-fill:not(.filled)[data-index="51"]')
+
+    await u.setValue('#slider-1 input', '51')
+    // Slider fill segment 51 should be filled, but not 52
+    await u.findElement('#slider-1 .slider-fill.filled[data-index="51"]')
+    await u.findElement('#slider-1 .slider-fill:not(.filled)[data-index="52"]')
+  })
+
   it('assigns data-values to a hidden input and labels', async () => {
+    // Check that value and label match
     await u.matchText('#slider-2', 'poor')
     await u.valueIs('#slider-2 input[type="hidden"]', 'poor')
-    
+
+    // Move the slider
     await u.setValue('#slider-2 input', '1')
+
+    // Check that value and label update and match
     await u.matchText('#slider-2', 'fair')
     await u.valueIs('#slider-2 input[type="hidden"]', 'fair')
-
   })
 
   it('adds marks', async () => {
+    // There should only be three slider marks
     await u.countIs('#slider-3 .slider-segment-mark', 3)
-    
+
+    // Marked elements should occupy the right place in the DOM and have the right data-index
     await u.findElement(`#slider-3 .slider-segment:first-child [data-index='1']`)
     await u.findElement(`#slider-3 .slider-segment:nth-child(6) [data-index='6']`)
     await u.findElement(`#slider-3 .slider-segment:last-child [data-index='11']`)
@@ -61,4 +87,13 @@ describe('Browser slider testing', () => {
   it('sets max and min from attributes', async () => {
     await u.countIs('#slider-4 .slider-segment', 4)
   })
+
+  it('disables the label', async () => {
+    // data-label='false' should prevent the label from showing
+    await u.matchText('#slider-5', '')
+  })
+
+  // TODO: Line lables
+  // TODO: External Labels
+  // TODO: External state tracking: data-track-slider-state="#sliderid"
 })
